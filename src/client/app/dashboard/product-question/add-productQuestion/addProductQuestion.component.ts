bf7b0ccc70b5cca48babe7d74a3162b3d26bf659
rekha <rekha4395@gmail.com>
2017-01-30 23:29:23 -0800

    Add Product Questions Module

--- src/client/app/dashboard/product-question/add-productQuestion/addProductQuestion.component.ts
+++ src/client/app/dashboard/product-question/add-productQuestion/addProductQuestion.component.ts
@@ -5,29 +5,34 @@ import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
 	moduleId: module.id,
-	selector: 'provider-cmp',
+	selector: 'productQuestion-cmp',
 	templateUrl: 'addProductQuestion.component.html'
 })
 
 export class AddProductQuestionComponent {
 	model: any= {};
+	token:any[];
+    message: any= {};
+	mess = false;
 
+	token = localStorage.getItem('access_token');
 	constructor(private http : Http, private router: Router, private route: ActivatedRoute) {}
 
 	ngOnInit() {
-	   this.route.queryParams.subscribe(data => {this.model.id = data['id'], this.model.title = data['title']});
+	   this.route.queryParams.subscribe(data => {this.model.id = data['id'], this.model.title = data['title'],this.model.description = data['description']});
   	}
 	
-	reset() {
+	save() {
+		this.model.type = "product"
 		let headers = new Headers();
   		headers.append('content-Type', 'application/json');
-    	this.http.post('http://54.161.216.233:8090/api/secured/cms-pages?access_token=c1417477-6f4b-485e-a518-f3de5cbca17e', this.model, {headers: headers})
+    	this.http.post('http://54.161.216.233:8090/api/secured/question?access_token=' + this.token, this.model, {headers: headers})
 			.map(res => res.json())
 			.subscribe(
-				data =>  {this.router.navigate(['/dashboard/ProductQuestion'])},
+				data =>  {this.router.navigate(['/dashboard/productQuestion'])},
 				error => console.log("error"),
-  				() => this.ngOnInit()
-			);
+  				() => console.log("complete");
+  			);
 	}
 
 }
--- src/client/app/dashboard/product-question/productQuestion.component.ts
+++ src/client/app/dashboard/product-question/productQuestion.component.ts
@@ -1,39 +1,56 @@
 import {Component } from '@angular/core';
 import { Http } from '@angular/http';
+import { PagerService } from '../pager.service'
 import { Router } from '@angular/router';
-import {FroalaEditorComponent} from 'ng2-froala-editor/ng2-froala-editor';
-
 
 @Component({
 	moduleId: module.id,
 	selector: 'product-question',
 	templateUrl: 'productQuestion.component.html'
+
+	providers: [PagerService]
+
 })
 
 export class ProductQuestionComponent {
+	productQuestions: Array<Object>[];
+	pager: any = {};
+	terms:string = '';
+    pagedItems: any[];
+    token:any[];
+    message: any= {};
+	mess = false;
+
+	token = localStorage.getItem('access_token');
 	
-	/*productQuestions: Array<Object>[];
-	showData: false;
+	constructor(private http : Http, private pagerService : PagerService,private router: Router) { }
 
-	constructor(private http : Http, private router : Router) { }
-
-	/*ngOnInit() {
-		this.http.get('http://54.161.216.233:8090/api/secured/cms-pages?access_token=c1417477-6f4b-485e-a518-f3de5cbca17e')
+	ngOnInit() {
+		var type = "product";
+		this.http.get('http://54.161.216.233:8090/api/secured/question/type/'+ type +'?access_token=' + this.token)
   				.map(res => res.json())
   				.subscribe(
-  					data => this.productQuestions =data.content,
-  					error => console.log("error"),
-  					() => console.log(this.productQuestions);
+  					data => { if(data.length) {
+                  				this.productQuestions= data;
+                  				this.setPage(1);
+                  			} else {
+                      			this.mess=true;
+                      			this.message= "There is no records found."
+                  			}},
+  					error => { if(error.json().error) {
+									this.message = error.json().message
+									this.mess = true;
+								}},
+  					() => console.log("complete")
   				);
-	}*/
-	
-	/*add() {
-		
-		this.router.navigate(['/dashboard/add-productQuestion/']);
+	}
+
+	add() {
+		this.router.navigate(['/dashboard/add-ProductQuestion/']);
 	}
 
 	delete(id: number) {
-    	this.http.delete('http://54.161.216.233:8090/api/secured/cms-pages/' + id + '?access_token=c1417477-6f4b-485e-a518-f3de5cbca17e')
+    	this.http.delete('http://54.161.216.233:8090/api/secured/question/' + id + '?access_token=' + this.token)
 			.map(res => res.json())
 			.subscribe(
 				data => console.log(data),
@@ -42,7 +59,30 @@ export class ProductQuestionComponent {
 			);
     }
 
-    update(id: number, title: string) {
-    	this.router.navigate(['/dashboard/add-ProductQuestion/'], { queryParams: { id: id, title: title}});
-    }*/
-   }
+    update(id: number, title: string, description:string, isDefault:boolean) {
+    	if(isDefault) {
+    		this.mess=true;
+            this.message= "Can't Edit! These is default question."
+    	} else {
+    		this.router.navigate(['/dashboard/add-ProductQuestion/'], { queryParams: { id: id, title: title, description: description}});
+    	}
+    }
+
+	search(terms: string) {
+		console.log(terms)
+		if(terms) {
+			this.pagedItems = this.productQuestions.filter((item) => item.title.startsWith(terms));
+		} else {
+			this.ngOnInit();
+		}
+	}
+   
+    setPage(page: number) {
+        if (page < 1 || page > this.pager.totalPages) {
+            return;
+        }
+        this.pager = this.pagerService.getPager(this.productQuestions.length, page);
+        this.pagedItems = this.productQuestions.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    }   
+}
+
