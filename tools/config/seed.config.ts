e28e921f0156431fd43f518b43d4ae97f2af0f38
Shankar Palsaniya <shankar.dayal@purgesoftwares.com>
2017-02-01 09:18:14 +0530

    Change title and serach box zindex

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -144,7 +144,7 @@ export class SeedConfig {
    * `index.html`.
    * @type {string}
    */
-  APP_TITLE = 'Welcome to angular2-seed!';
+  APP_TITLE = 'Admin Panel: Coupon redemption app!';
 
   /**
    * The base folder of the applications source files.
