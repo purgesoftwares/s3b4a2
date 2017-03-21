6f78c1a1ff326d7d65ab264ad89ce558c8527f30
rekha <rekha.agarwal@xtreemsolution.com>
2017-03-21 14:46:14 +0530

    Add consumer table with table library

--- src/client/app/dashboard/consumers/consumer.module.ts
+++ src/client/app/dashboard/consumers/consumer.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { DataTableModule } from 'angular-2-data-table';
 
 import { ConsumerComponent } from './consumer.component';
 
 @NgModule({
-    imports: [RouterModule,BrowserModule, FormsModule],
+    imports: [RouterModule,BrowserModule, FormsModule, DataTableModule],
     declarations: [ConsumerComponent],
     exports: [ConsumerComponent]
 })
